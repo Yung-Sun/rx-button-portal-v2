e9855143247b20a7dc20b28e8e4be47aae3100c7
Yungsun <yungsun3307@163.com>
2022-01-22 14:37:06 +0800

    feat: 🥳 Complete the Start page

--- src/views/TheStart.vue
+++ src/views/TheStart.vue
@@ -1,15 +1,68 @@
 <template>
-  <div>
-    <h1>Start Page</h1>
+  <div class="theStart">
+    <div>
+      <p>RX-Button作为组件使用非常简单，只需要
+        <router-link to="/install">安装</router-link>
+        然后在项目中引入，即可轻松使用。
+      </p>
+      <section>
+        <h2>全局引入</h2>
+        <pre v-highlight><code class="Javascript" style="border-radius: 10px;">{{ globalImport }}</code></pre>
+        <h2>局部引入</h2>
+        <pre v-highlight><code class="Javascript" style="border-radius: 10px;">{{ componentImport }}</code></pre>
+      </section>
+      <footer>
+        <router-link to="/install">
+          <a-icon type="left"/>
+          安装说明
+        </router-link>
+        <router-link to="/guide">使用指南
+          <a-icon type="right"/>
+        </router-link>
+      </footer>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TheStart"
+  name: "TheStart",
+  data() {
+    return {
+      globalImport: `  // main.js
+  import Vue from 'vue'
+  import App from './App.vue'
+
+  import RXButton from 'rx-button'
+  Vue.component('rx-button', RXButton)
+
+  new Vue({
+    render: h => h(App)
+  }).$mount('#app')`,
+      componentImport: `  // xxx.vue 某个组件
+  import RXButton from 'rx-button'
+
+  export default {
+    components:{ 'rx-button': RXButton }
+  }`
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.theStart {
+  height: 100%;
+}
 
+footer {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+  margin-top: 50px;
+
+  > span {
+    cursor: pointer;
+  }
+}
 </style>
